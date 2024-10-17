b97edcbe44cb24083ba16d17ba5cb402097ff558
mightyfleaaa and Xpl0its_Bop2 <e21.jan.orvalencia@suizoamericano.edu.gt>
2024-10-17 14:23:57 -0600

    -

--- loadstring.lua
+++ loadstring.lua
@@ -2,6 +2,8 @@
 local tcs = game:GetService("TextChatService")
 if tcs.ChatVersion == "LegacyChatService" then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/sitiosweb24/Chat-Thingy-GUI-FE-Script/refs/heads/main/textchatservice.lua"))()
+  print(tcs.ChatVersion)
 elseif tcs.ChatVersion == "TextChatService" then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/sitiosweb24/Chat-Thingy-GUI-FE-Script/refs/heads/main/oldchatservice.lua"))()
+  print(tcs.ChatVersion)
 end
