c7c19489131fbff365c14dba8fd86987680cedec
mightyfleaaa and Xpl0its_Bop2 <e21.jan.orvalencia@suizoamericano.edu.gt>
2024-10-17 20:25:23 -0600

    -

--- loadstring.lua
+++ loadstring.lua
@@ -1,9 +1,9 @@
 -- copy this and paste it in executor.
 local tcs = game:GetService("TextChatService")
-if tcs.ChatVersion == "LegacyChatService" then
+if tcs.ChatVersion == Enum.ChatVersion.TextChatService then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/sitiosweb24/Chat-Thingy-GUI-FE-Script/refs/heads/main/textchatservice.lua"))()
   print(tcs.ChatVersion)
-elseif tcs.ChatVersion == "TextChatService" then
+elseif tcs.ChatVersion == Enum.ChatVersion.LegacyChatService then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/sitiosweb24/Chat-Thingy-GUI-FE-Script/refs/heads/main/oldchatservice.lua"))()
   print(tcs.ChatVersion)
 end
